9fbd887db8aec5804c74ae05ea68906826803629
Alan Emiliano Ramirez Ayala <75821310+Replase@users.noreply.github.com>
2022-07-19 11:10:22 -0500

    Initial Commit

---
+++ MACNoticias/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MACNoticias
+//
+//  Created by Alan Emiliano Ramirez Ayala on 12/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
